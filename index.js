b384cd027d6978e2605e15d779c8323e4a91a8d5
TeresaFlores <teresaflores1730@gmail.com>
2019-12-05 16:23:48 -0600

    Petición Fetch

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// const data= response_marvel.json;
-// console.log (data)
 
-// for (i in data.results){
+fetch('response_marvel.json').then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    console.log(data);
+  }).catch(err => {
+    // Do something for an error here
+  });
 
-// }
-
-// console.log(data.code);
-const  marvel= new MARVELRequest();
-
-
-console.log(marvel)
-
-// fetch('./Gluo/response_marvel.JSON')
-//  .then (response => response.json () )
-//  .then(data => {
-//      console.log(data)
-//  })
-//  .catch (err => console.log (err))
-
-// const dataMarvel= JSON.parse(results);
-// console.log(dataMarvel.code)
 
 

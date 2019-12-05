9644bd6af32bfec400b6baf97adcc0d87fd2f914
TeresaFlores <teresaflores1730@gmail.com>
2019-12-05 14:25:01 -0600

    cambios

---
+++ index.js
@@ -0,0 +1,24 @@
+// const data= response_marvel.json;
+// console.log (data)
+
+// for (i in data.results){
+
+// }
+
+// console.log(data.code);
+const  marvel= new MARVELRequest();
+
+
+console.log(marvel)
+
+// fetch('./Gluo/response_marvel.JSON')
+//  .then (response => response.json () )
+//  .then(data => {
+//      console.log(data)
+//  })
+//  .catch (err => console.log (err))
+
+// const dataMarvel= JSON.parse(results);
+// console.log(dataMarvel.code)
+
+
